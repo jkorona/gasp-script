db5d8ec5644b36f3713d6176fa00c5f10faec5ae
Jan Korona <jan@withlocals.com>
2022-01-14 15:06:05 +0100

    basic functionality ready

--- index.mjs
+++ index.mjs
@@ -1,16 +1,44 @@
 
+import { resolve, basename } from 'path';
+import { mkdirSync, copyFileSync } from 'fs';
 import { read } from './lib/utils/reader.mjs';
 import { sort } from './lib/utils/sorter.mjs';
 
 const options = {
   group: 'month', // 'none' | 'month',
-  sort: 'asc', // 'asc' | 'dsc' 
+  sort: 'asc', // 'asc' | 'dsc',
+  mode: 'copy',  // copy | move
 };
 
-const files = await read(process.argv[2]);
+const source = process.argv[2];
+const target = process.argv[3]
+
+const files = await read(source);
 const sorted = sort(files, options);
 
-console.log(JSON.stringify(sorted, null, 1));
+Object.keys(sorted).forEach((year) => {
+  const yearDir = resolve(target, year);
+  mkdirSync(yearDir, { recursive: true });
+
+  Object.keys(sorted[year]).forEach((month) => {
+    const monthDir = resolve(yearDir, month);
+    mkdirSync(monthDir, { recursive: true });
+
+    Object.keys(sorted[year][month]).forEach((day) => {
+      const dayDir = resolve(monthDir, day);
+      mkdirSync(dayDir, { recursive: true });
+
+      sorted[year][month][day].forEach(({ name: oldPath }) => {
+        const newPath = resolve(dayDir, basename(oldPath));
+        copyFileSync(oldPath, newPath);
+      });
+    });
+
+  });
+});
+
+
+// console.log(JSON.stringify(sorted, null, 1));
 
 
 // const fs = require('fs');
