518d76478c030b549a64d249cc7a95f78c0e2ca8
Jan Korona <jan@withlocals.com>
2022-11-25 22:56:42 +0100

    add grouping option

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Script follows algorithm described below:
 
 1. Finds all the images in the `source` folder and all its subfolders
 2. Reads photo creation date and time from file metadata (exif)
-3. Sorts by date/time and groups by year, month and day
+3. Sorts by date/time and groups by year, month, day or outputs flat list
 4. Saves photos in new ordered file structure under `target` location
 
 Currently supported file types:
@@ -27,12 +27,13 @@ npm install -g gasp-script
 Usage: gasp [options] <source> <target>
 
 Arguments:
-  source             path to directory with photos to group & sort
-  target             path to directory where grouped & sorted photos should be placed afterwards
+  source                 path to directory with photos to group & sort
+  target                 path to directory where grouped & sorted photos should be placed afterwards
 
 Options:
-  -V, --version      output the version number
-  -s, --sort <dir>   sorting direction (choices: "asc", "dsc", default: asc)
-  -f, --flat         output flat folder structure
-  -h, --help         display help for command
+  -V, --version          output the version number
+  -s, --sort <dir>       sorting direction (choices: "asc", "dsc", default: asc)
+  -v, --verbose          enables additional logging dor debugging
+  -g, --groupby <level>  group photos by year, month, day, none (choices: "year", "month", "day", "none", default: none)
+  -h, --help             display help for command
 ```
--- index.mjs
+++ index.mjs
@@ -1,14 +1,10 @@
 #!/usr/bin/env node
-
-import { resolve, extname } from 'path';
-import { mkdirSync, copyFileSync } from 'fs';
 import { promisify } from 'util';
 import figlet from 'figlet';
 import { program, Option } from 'commander/esm.mjs';
 import { read } from './lib/utils/reader.mjs';
 import { sort } from './lib/utils/sorter.mjs';
-import { progress } from './lib/utils/progress.mjs';
-import { flatten } from './lib/utils/flatten.mjs';
+import { write } from './lib/utils/writer.mjs';
 
 const log = (...messages) => options.verbose && console.log(...messages);
 const logo = await promisify(figlet)('GASP', { font: 'Speed' });
@@ -19,8 +15,8 @@ program
   .argument('<source>', 'path to directory with photos to group & sort')
   .argument('<target>', 'path to directory where grouped & sorted photos should be placed afterwards')
   .addOption(new Option('-s, --sort <dir>', 'sorting direction').choices(['asc', 'dsc']).default('asc', 'asc'))
-  .addOption(new Option('-f, --flat', 'output flat folder structure'))
-  .addOption(new Option('-v, --verbose', 'enables additional logging dor debugging'));
+  .addOption(new Option('-v, --verbose', 'enables additional logging dor debugging'))
+  .addOption(new Option('-g, --groupby <level>', 'group photos by year, month, day, none').choices(['year', 'month', 'day', 'none']).default('none', 'none'));
 
 program.parse(process.argv);
 
@@ -35,28 +31,5 @@ log(`Found ${files.length} files in source directory ${source}.`);
 const sorted = sort(files, options);
 log('Sort tree:', JSON.stringify(sorted, '', 1));
 
-const update = progress(files.length, options.verbose);
-
-function build(model, target) {
-  if (Array.isArray(model)) {
-    model.forEach(({ name: oldPath }, index) => {
-      const newPath = resolve(target, index + 1 + extname(oldPath).toLowerCase());
-      copyFileSync(oldPath, newPath);
-      update();
-    });
-  } else {
-    Object.keys(model).forEach((dir) => {
-      const dirPath = resolve(target, dir);
-      mkdirSync(dirPath, { recursive: true });
-      build(model[dir], dirPath);
-    });
-  }
-}
-
-if (options.flat) {
-  build(flatten(sorted), target);
-} else {
-  build(sorted, target);
-}
-
+write(sorted, target, files.length, options);
 console.log('Done!\n');
